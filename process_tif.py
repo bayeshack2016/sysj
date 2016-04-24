512e595f42510cf1bd97636d5615a87049f04fbe
Jeff Wu <wuthefwasthat@gmail.com>
2016-04-23 17:19:46 -0700

    blah

---
+++ process_tif.py
@@ -0,0 +1,5 @@
+tiff_file = '/Users/jeffwu/Documents/SVDNB_npp_20160202-20160229_75N180W_vcmcfg_v10_c201603152010/SVDNB_npp_20160201-20160229_75N180W_vcmcfg_v10_c201603152010.avg_rade9.tif'
+'/Users/jeffwu/Documents/SVDNB_npp_20160202-20160229_75N180W_vcmcfg_v10_c201603152010/SVDNB_npp_20160201-20160229_75N180W_vcmcfg_v10_c201603152010.avg_rade9.small.tif'
+
+import matplotlib.pyplot as plt
+I = plt.imread(tiff_file)
