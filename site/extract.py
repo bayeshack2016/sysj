75039c6f7b4d7eda367009d57b12c243630c33ba
Sasha <sasha.targ@gmail.com>
2016-04-23 23:53:15 -0700

    create dictionary with statistics for each county

---
+++ site/extract.py
@@ -0,0 +1,28 @@
+import rasterio
+import numpy as np
+import pandas as pd
+
+# relative import
+import data
+
+dao = data.Data()
+
+counties = dao.counties[:5]
+
+def get_county_data(counties, month):
+    percents = np.linspace(0, 100, 101)
+    for n in counties:
+        _, _, unmasked, masked = dao.get_county(n, month, 'raster')
+        county_name, state_name = tuple(n.split(','))
+        # import pudb; pudb.set_trace()
+        flat = np.array(masked[~masked.mask])
+        percentiles = np.percentile(flat, percents)
+        county_data = {
+            'name': n,
+            'county_name': county_name,
+            'state_name': state_name,
+        }
+        for p, v in zip(percents, percentiles):
+            county_data['percentile_%d' %p] = v
+        yield county_data
+    
